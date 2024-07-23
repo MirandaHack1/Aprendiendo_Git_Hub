575ad9ee9ffd148fda0780134fc144a0dcfb7b71
DevJhonny <miranda3791167@gmail.com>
2024-07-23 18:02:00 -0500

    CREANDO ARCHIVO STYLES Y ACTULIZANDO CONTENIDO DEL INDEX Y ACTUALIZANDO COMANDOS

--- index.php
+++ index.php
@@ -1,11 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-    
+    <h1>MI NOMBRE: JHONNY MIRANDA</h1>
+    <h1>MI APELLIDO: MIRANDA</h1>
+
 </body>
+
 </html>
